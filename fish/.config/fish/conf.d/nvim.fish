c83828a31c8468f50373b840fdfe41f328ccefc0
René van Asperen <rvanasperen@gmail.com>
2025-02-08 18:23:36 +0100

    Update fish configs

--- fish/.config/fish/conf.d/nvim.fish
+++
@@ -1,2 +0,0 @@
-set -gx EDITOR nvim
-set -gx VISUAL nvim
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,11 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    set -U fish_greeting
+status is-interactive || return
+
+if test -d /opt/homebrew; then
+    fish_add_path /opt/homebrew/bin
+    fish_add_path /opt/homebrew/sbin
 end
+
+set -U fish_greeting
+
+set -gx EDITOR nvim
+set -gx VISUAL nvim
