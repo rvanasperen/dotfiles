349b667b10909fff276fa932723f96f1ee3ee5ab
René van Asperen <rvanasperen@gmail.com>
2025-03-03 11:59:05 +0100

    Use LazyVim for nvim configuration

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,21 +1,3 @@
--- Gutter
-vim.o.number = true
-vim.o.relativenumber = true
-
--- Indentation
-vim.o.expandtab = true  -- Insert spaces instead of a tab character when pressing <Tab>
-vim.o.tabstop = 4       -- Amount of columns occupied by a tab character
-vim.o.shiftwidth = 4    -- Amount of columns used to indent new lines
-
--- Clipboard
-vim.o.clipboard = "unnamedplus"
-
--- Leader
-vim.g.mapleader = " "
---vim.g.maplocalleader = " "
-
--- Search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.hlsearch = false
-
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
