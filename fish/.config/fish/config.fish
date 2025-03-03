d627cd73c5d2107cb4c1f43db0708cb76c483896
René van Asperen <rvanasperen@gmail.com>
2025-03-03 11:28:27 +0100

    Add fallback for 'ls' command to use 'eza' if available

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,5 +3,9 @@ status is-interactive || return
 set -U fish_greeting
 set -U fish_key_bindings fish_vi_key_bindings
 
+if command -q eza
+    set -U __fish_ls_command eza
+end
+
 set -gx EDITOR nvim
 set -gx VISUAL nvim
