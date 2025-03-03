4361f88e80156edd8b562ad03288cc0d019f6873
René van Asperen <rvanasperen@gmail.com>
2025-03-03 11:19:31 +0100

    Enhance 'cat' function to support 'batcat' fallback

--- fish/.config/fish/functions/cat.fish
+++ fish/.config/fish/functions/cat.fish
@@ -1,3 +1,10 @@
-function cat --wraps=bat
-    bat $argv
+function cat
+    if command -q batcat
+        batcat $argv
+    else if command -q bat
+        bat $argv
+    else
+        command cat $argv
+    end
 end
+
