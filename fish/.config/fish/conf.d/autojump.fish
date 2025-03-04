4b0eac845aa448eb81b9c9ccb70569ff68028e83
René van Asperen <rvanasperen@gmail.com>
2025-03-04 09:55:56 +0100

    Fix autojump load script

---
+++ fish/.config/fish/conf.d/autojump.fish
@@ -0,0 +1,7 @@
+if test -f /usr/share/autojump/autojump.fish
+    . /usr/share/autojump/autojump.fish
+end
+
+if test -f /opt/homebrew/share/autojump/autojump.fish
+    . /opt/homebrew/share/autojump/autojump.fish
+end
--- fish/.config/fish/conf.d/homebrew-autojump.fish
+++
@@ -1,3 +0,0 @@
-test -f /opt/homebrew/share/autojump/autojump.fish || return
-
-source /opt/homebrew/share/autojump/autojump.fish
