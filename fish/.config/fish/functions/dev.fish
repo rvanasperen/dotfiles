2078a36c8d1713ea4bd3fa39f7f5d1abe0a3383c
René van Asperen <rvanasperen@gmail.com>
2025-03-17 09:39:37 +0100

    Update `dev` Fish command

--- fish/.config/fish/functions/dev.fish
+++ fish/.config/fish/functions/dev.fish
@@ -1,8 +1,19 @@
 function dev
-    if ! test -f ./artisan
-        echo "Not in a Laravel app directory"
+    # Laravel
+    if test -f ./artisan
+        if test -f ./vendor/bin/sail && test -f ./docker-compose.yml
+            echo "Starting Laravel app via Sail"
+            ./vendor/bin/sail up -d
+            ./vendor/bin/sail npm run dev
+            ./vendor/bin/sail down
+        else
+            echo "Starting Laravel app"
+            composer dev
+        end
+
+    # Unknown
+    else
+        echo "Unknown project type"
         return 1
     end
-
-    composer dev $argv
 end
